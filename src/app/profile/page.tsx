7d22e0fcbf14017755a3d79fc74f787c33926c74
Nida <nnpervez333@gmail.com>
2024-12-20 15:33:22 +0500

    change

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,12 +5,12 @@ const Pagep = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 font-mono font-bold text-xl sm:text-2xl md:text-3xl'>
       <p className='text-center px-4 sm:px-6'>
-        Congratulations, you are logged in.Welcome to the Editable Blogging website .
+        Congratulations, you are logged in.Welcome to the Blogging website .
       </p>
 
       <div className='mt-8 space-y-4'>
         <button className='w-full sm:w-auto bg-purple-600 text-white rounded-lg py-3 px-6 text-center'>
-          <Link href="https://crudblogging-git-main-nida-pervezs-projects.vercel.app/">
+          <Link href="https://dynamicblogweb.vercel.app/">
             Click here to open the website
           </Link>
         </button>
